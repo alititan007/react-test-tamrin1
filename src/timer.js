696a86fd0ac5fd4d52398ba8dc7b2c757db4002b
pedram.mb <pedram.mb3142@gmail.com>
2021-11-04 13:25:56 +0330

    test1

---
+++ src/timer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+var inter;
+class Timer extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            Second : 8,
+            minute : 1,
+            hour : 0
+        }
+    }
+    componentDidMount(){
+        inter = setInterval(()=>{
+            if(this.state.Second > 0){
+                this.setState({
+                Second : this.state.Second -1
+                })
+                if(this.state.Second == 0){
+                    this.setState({
+                        minute : this.state.minute -1
+                        //Second : 8
+                    })
+                }
+            }
+        } , 1000)
+    }
+    componentDidUpdate(){
+        if(this.state.minute == 0){
+            clearInterval(inter)
+        }
+    }
+    render(){
+        return(
+            <h2 className="timer">
+                it is{this.state.hour}:{this.state.minute}:{this.state.Second}
+            </h2>
+        )
+    }
+}
+export default Timer;
